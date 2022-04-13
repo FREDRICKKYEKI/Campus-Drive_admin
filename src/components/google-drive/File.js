a1ea15156df9d285f6b26c1545182195e5a04a88
FREDRICKKYEKI <101971767+FREDRICKKYEKI@users.noreply.github.com>
2022-04-13 09:57:06 +0300

    Update File.js

--- src/components/google-drive/File.js
+++ src/components/google-drive/File.js
@@ -1,9 +1,85 @@
-import { faFile } from "@fortawesome/free-solid-svg-icons"
+import {React, useState} from "react"
+import { Card, Nav, Modal, Button } from "react-bootstrap"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import React from "react"
+import { faFile, faTrashCan} from "@fortawesome/free-solid-svg-icons"
+import { database } from "../../firebase"
+import { storage } from "../../firebase"
+import {Document, Page} from "react-pdf"
+import { pdfjs } from "react-pdf"
 
-export default function File({ file }) {
+export default function File({ file}) {
+  const [numPages, setNumPages] = useState(null);
+  const [modalShow, setModalShow] = useState(false);
+ 
+  pdfjs.GlobalWorkerOptions.workerSrc =  `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+   
+    const [pageNumber, setPageNumber] = useState(1);
+  
+    function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);}
+
+    function handleDelete(){
+      setModalShow(false)
+      var desertRef = storage.refFromURL(file.url)
+      desertRef.delete().then(()=>{
+        console.log("Document deleted successfully")
+      }).catch((err)=>{
+        console.log("An error occured while deleting document")
+        console.log("Error:" + err.message)
+      })
+      database.files.doc(file.id).delete().then(()=>{
+        console.log("Document deleted successfully")
+      }).catch((err)=>{
+        console.log("An error occured while deleting document")
+        console.log("Error:" + err.message)
+      })
+  }
+  
+  function MyVerticallyCenteredModal(props) {
+    return (
+      
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Delete File
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Are you sure you want to delete?</h4>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.onHide}>Close</Button>
+          <Button variant="danger" onClick={()=> handleDelete()}>Delete</Button>
+        </Modal.Footer>
+      </Modal>
+    ); 
+  }
+  
   return (
+    <>
+       <Card>
+      <Card.Header>
+      <Nav className="container-fluid">
+          <Nav.Item className="ms-auto">
+            <FontAwesomeIcon icon={faTrashCan} onClick={() => setModalShow(true)} />
+          </Nav.Item>
+        </Nav>
+      </Card.Header>
+      {/* <Card.Body>
+       <Document
+        file={file.url}
+        onLoadSuccess={onDocumentLoadSuccess}
+        >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      </Card.Body> */}
+      <Card.Footer>
     <a
       href={file.url}
       target="_blank"
@@ -13,5 +89,14 @@ export default function File({ file }) {
       <a style={{ marginLeft: '.5rem' }} />
       {file.name}
     </a>
+   
+      </Card.Footer>
+    </Card>
+    <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+   
   )
 }
